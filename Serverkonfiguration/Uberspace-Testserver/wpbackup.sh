c3f099dc835e7ea8f1b5805c564f8717e57b3595
Stefan Böhme <github@eichtal.net>
2017-01-21 22:20:32 +0100

    Backup-Skript um Ausnahme für wp-config ergänzt

--- Serverkonfiguration/Uberspace-Testserver/wpbackup.sh
+++ Serverkonfiguration/Uberspace-Testserver/wpbackup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # Dieser Skript zieht einen Dump aus der Datenbank und legt diesen
-# im Wordpress-Verzeichnis ab. Anschließend wird  das komplette
+# im Wordpress-Verzeichnis ab. Anschließend wird das komplette
 # Wordpress-Verzeichnis als Archiv gepackt und im Verzeichnis
 # ~/wp_backup hinterlegt.
 #
 # Als Eingabe erwartet der Skript ein Stichwort zur Kennzeichnung
 # des Archivs, z.B. ./wpbackup erstinstallation
+#
+# Standardmäßig wird die wp-config.php nicht mitgespeichert im Backup,
+# da hier Passwörter hinterlegt sind. Mit dem Schalter "wp-config" kann
+# diese in das Backup eingeschlossen werden, z.B.
+# ./wpbackup erstinstallation wp-config
+#
 
+
+# Variablen für Dateinamen und Pfade anlegen
+# $1 enthält eine Kurzbezeichnung zur Charakterisierung des Backups
 NOW=$(date +"%Y-%m-%d-%H%M")
 FILE="$NOW.$1.tar.gz"
 
@@ -18,8 +27,24 @@ DB_USER=$USER
 DB_NAME=$USER
 DB_FILE="$NOW.$1.sql"
 
+
+# Überprüfen, ob Backup-Verzeichnis vorhanden ist und ggf. anlegen
 test -d $BACKUP_DIR || mkdir $BACKUP_DIR
+
+# Verzeichnis für temporäre Zwischenspeicherung des Backups anlegen
 mkdir $WWW_DIR/database_backup
-mysqldump -u$DB_USER $DB_NAME > /var/www/virtual/$USER/$WP_DIR/database_backup/$DB_FILE
-tar -C $WWW_DIR -vczf $BACKUP_DIR/$FILE .
+
+# Kopie der Datenbank im Wordpress-Verzeichnis anlegen
+mysqldump -u $DB_USER $DB_NAME > /var/www/virtual/$USER/$WP_DIR/database_backup/$DB_FILE
+
+# Prüfen, ob die wp-config mitgespeichert werden soll
+if [ "$2" == "wp-config" ]
+	then
+		tar -C $WWW_DIR -vczf $BACKUP_DIR/$FILE .
+	else
+		tar -C $WWW_DIR -vczf $BACKUP_DIR/$FILE . --exclude="wp-config.php"
+fi
+
+# Temporäres Verzeichnis wieder löschen
 rm -r $WWW_DIR/database_backup
+
